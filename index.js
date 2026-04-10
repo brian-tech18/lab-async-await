a9bcaa69e6b249d91baf29e90f85541c78e8a0e0
brian-tech18 <briankimani432@gmail.com>
2026-04-10 19:10:45 +0300

    completed bookstore manager lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your code here!
+function showPostsOnScreen(postArray) {
+  const listContainer = document.getElementById('post-list');
+
+  for (const singlePost of postArray) {
+    const listItem = document.createElement('li');
+    
+    const postTitle = document.createElement('h1');
+    postTitle.textContent = singlePost.title;
+
+    const postContent = document.createElement('p');
+    postContent.textContent = singlePost.body;
+
+    listItem.appendChild(postTitle);
+    listItem.appendChild(postContent);
+
+    listContainer.appendChild(listItem);
+  }
+}
+
+async function startSocialMediaApp() {
+  try {
+    
+    const apiResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+    
+    const results = await apiResponse.json();
+
+    showPostsOnScreen(results);
+
+  } catch (err) {
+    console.warn("Could not load the posts:", err);
+  }
+}
+
+startSocialMediaApp();
